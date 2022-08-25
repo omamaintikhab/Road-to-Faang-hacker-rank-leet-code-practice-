4c1f54e1a10ad1086da2cc3b0643565035cf830b
omamaintikhab <omamaintikhab97@gmail.com>
2022-08-25 23:44:04 +0500

    min_max_sum problem

--- min_max_sum.py
+++ min_max_sum.py
@@ -6,6 +6,15 @@ O(n) => iterating over a loop of n
 O(n^2) => 2 for loops with n number of inputs
 '''
 
+'''
+My Approach:
+since we need to tell the minimum and maximum sum that could be made from the given array of five
+1. sort the array
+2. initiat min and max sum to 0
+3. run a loop for a range five 
+4. for minimum skip the last index
+5. for max skip the first index
+'''
 def min_max_sum(arr):
     arr.sort()
     min_sum = 0
@@ -15,6 +24,6 @@ def min_max_sum(arr):
             min_sum+=arr[i]
         if i !=0:
             max_sum+=arr[i]
-    return min_sum, max_sum
+    print(str(min_sum)+" "+str(max_sum))
 
 print(min_max_sum([1,3,5,7,9]))
